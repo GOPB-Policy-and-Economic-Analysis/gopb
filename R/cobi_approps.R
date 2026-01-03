ec09b0e7b0d9cd7d8d19cbea9f8679477dcfb0ec
bolos-gopb <jjbolos@utah.gov>
2026-01-02 17:45:54 -0700

    Improved function-format robustness of test cobi_approprs function.

--- R/cobi_approps.R
+++ R/cobi_approps.R
@@ -1,8 +1,17 @@
 view <- 'appropriations' # null, appropriations, expected_expenditures
 
-years <- c(2024, 2025) # year(s) of interest - numeric list
 
 # NOT YET PUTTING A LOT OF THIS REPETITIVE CODE INTO ANOTHER FUNCTION LIKE cobi_query() OR SOMETHING UNTIL I CAN SEE WHAT ALL IS TRULY REPRODUCIBLE WITH THE INCLUSION OF THE VARIOUS USES/FILTERS DOWN THE ROAD
+
+#' Pull COBI appropriations
+#'
+#' @param years A vector of integers indicated year(s) of interest
+#'
+#' @returns A tibble (modern data frame)
+#' @export
+#'
+#' @examples
+#' cobi_data <- cobi_approps(c(2024, 2025))
 cobi_approps <- function(years) {
   # drop back to when first year of interest was the out-year to capture initial appropriations
   init_url <- appr_url(min(years) - 1)
@@ -28,7 +37,7 @@ cobi_approps <- function(years) {
 }
 
 
-cobi_expected_expenditures <- function(years) {
+cobi_expected_expenditure <- function(years) {
   # drop back to when first year of interest was the out-year to capture initial appropriations
   init_url <- appr_url(min(years) - 1)
 
@@ -49,5 +58,5 @@ cobi_expected_expenditures <- function(years) {
   # filter out preceding year and to appropriations view
   data %<>%
     dplyr::filter(FY %in% years) |>
-    dplyr::filter(CatType == 1)
+    dplyr::filter(CatType == 2)
 }
