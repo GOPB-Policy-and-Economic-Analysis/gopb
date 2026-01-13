02ad281e96e25601889963f9519915f25ef6cfe7
bolos-gopb <jjbolos@utah.gov>
2026-01-12 18:06:04 -0700

    almost finalized approps_dictionary with Brian and Kris.

--- data-raw/line_item_category_dictionary.R
+++
@@ -1,6 +0,0 @@
-line_item_category_dictionary <- tibble::tribble(
-  ~`Line Item Category` , ~Definition ,
-  ""                    , ""
-)
-
-usethis::use_data(line_item_category_dictionary, overwrite = TRUE)
